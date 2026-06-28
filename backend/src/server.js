826882ed8e23a5d43edfe207fd06cba6925dd91f
kakkaryogitaa <yogitakakkar100@gmail.com>
2026-06-28 13:30:37 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const envPath = path.resolve(__dirname, "../../.env");
+const envPath = path.resolve(__dirname, ".env");
 require("dotenv").config({ path: envPath });
 
 const app = require("./app");
