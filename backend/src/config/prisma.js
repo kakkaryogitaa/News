5cb166fcf6c7a4b4e28dae9b10a1b39bbb4878b5
kakkaryogitaa <yogitakakkar100@gmail.com>
2026-06-28 12:54:43 +0530

    Update prisma.js

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,8 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient({
-  log: ["error", "warn"]
-});
+const prisma = new PrismaClient();
+
+console.log("Prisma keys:");
+console.log(Object.keys(prisma));
 
 module.exports = prisma;
-
